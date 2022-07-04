db32a957f13949faba2329a03d7ba003a41ee858
Vers03888 <102573124+Vers03888@users.noreply.github.com>
2022-07-04 13:05:40 +1000

    Не получается создать калькулятор
    Не могу вывести значения в сроки и стоимость

--- Bootstrap/src/js/MyScript.js
+++ Bootstrap/src/js/MyScript.js
@@ -1,6 +1,6 @@
 "use strickt"
 
- let user_otvet = prompt("Выберите тип сайта", "Сайт-визитка, Интернет-магазин, Портфолио")
+/*let user_otvet = prompt("Выберите тип сайта", "Сайт-визитка, Интернет-магазин, Портфолио")
     if(user_otvet == "Сайт-визитка"){
       alert("+500р"); 
       }
@@ -14,7 +14,7 @@ else {
         alert("Выберите из предложенных вариантов"); 
         };
 
-let name = prompt("Выберите тип дизайна", "Maximum, Medium")
+let name = prompt("Выберите тип дизайна", "Minimal, Maximum, Medium")
 if(name == "Maximum"){
     alert("+1000р")
 }
@@ -34,8 +34,44 @@ if(adaptive == "Only mobile devices"){
 } 
 else{
         alert("Выберите из предложенных вариантов"); 
-        };
+        };*/
 
+
+/*(document).ready(function() {
+  $('.image-link').magnificPopup({type:'image'});
+});
+
+$("#carouselExampleControls").carousel({
+         interval : false
+});
+$("#carouselExampleControls1").carousel({
+         interval : false
+});
+
+$(document).ready(function() {
+setTimeout(function(){ 
+    $("#modal1").click();
+}, 20000);
+    });*/
+let price1 = document.querySelector('#price1');
+
+let price2 = document.querySelector('#price2');
+
+let price3 = document.querySelector('#price3');
+
+let inputArea = document.querySelector('#inputarea')
+
+
+const val1 = 2000;
+
+const val2 = 5000;
+
+const val3 = 4000;
+
+
+if(price1 == "#val1"){
+    val1.value = inputArea.value;
+}
 
 
 
