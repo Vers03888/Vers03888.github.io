3af19676d7ea4d81761c72a832d72aac1b15b4f9
Vers03888 <102573124+Vers03888@users.noreply.github.com>
2022-07-09 13:46:23 +1000

    Не могу разобраться, почему не отправляет данные

---
+++ visit/js/MyScript.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function() {
+  new WOW().init();
+});
+
+$(document).ready(function() {
+  $("#InputTel").mask("+7(999) 999-9999");
+});
---
+++ visit/php/test.php
@@ -0,0 +1,8 @@
+<?php
+
+$email = $_POST['user_email'];
+$phone = $_POST['user_phone'];
+$radio = $_POST['radio'];
+echo "скрипт сработал! <br>". $email . "<br>" . $phone . "<br>" . $radio ;
+
+?>
